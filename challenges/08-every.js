6e1460ff64945902cdc5343c9ed03d7671c905be
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-08 22:56:28 -0600

    ✅ challenge done

---
+++ challenges/08-every.js
@@ -0,0 +1,22 @@
+const team = [
+    {
+        name: 'Nicolas',
+        age: 12,
+    },
+    {
+        name: 'Andrea',
+        age: 8,
+    },
+    {
+        name: 'Zulema',
+        age: 2,
+    },
+    {
+        name: 'Santiago',
+        age: 18,
+    },
+];
+
+const rta = team.every((member) => member.age < 15);
+
+console.log('All members are younger than 15?', rta);
